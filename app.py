f11b78221475d46bea44c76c4fa35938ec27f3c4
Prajin Khatiwada <prajinkhatiwada@gmail.com>
2024-05-15 18:06:29 +0545

    this is simple login and signup sytem

---
+++ app.py
@@ -0,0 +1,16 @@
+username=input('enter username:')
+password=input('enter password:')
+
+
+
+print('acoount has been created')
+print('hey, login now!')
+
+username2=input('enter username:')
+password2=input('enter password:')
+
+if username==username2 and password==password2:
+    print('Logged in Successsfully')
+else:
+    print('invalid credentials')
+
